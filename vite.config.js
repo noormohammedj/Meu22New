2709cd9ca1602c8a2f61cdaebbf5489ba4ff9321
noormohammedj <noormohammed.j@gmail.com>
2025-08-16 14:11:22 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      external: ['react/jsx-runtime']
+    }
   },
   server: {
     port: 3000
